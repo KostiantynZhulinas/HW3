30d88279dca2eaedf079ff89c6f551fa9213c54d
KostiantynZhulinas <k.zhulinas@gmai.com>
2023-08-31 19:26:57 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const number = 33;
+
+if (number % 3 === 0 && number % 5 === 0) {
+    console.log('FizzBuzz');
+} else if (number % 3 === 0) {
+  console.log('Fizz');
+} else if (number % 5 === 0) {
+    console.log('Buzz');
+} 
+ 
+//var a;
+//a = 2023;
+
+//let accessAllowed;
+
+//if (a % 4 === 0) {
+//    accessAllowed = 'Так';
+//} else {
+//    accessAllowed = 'Ні';
+//}
+//console.log(accessAllowed);
+
