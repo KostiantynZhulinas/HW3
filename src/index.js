982931ae6745d3cb0ed8cd06080fa95087084e3b
KostiantynZhulinas <k.zhulinas@gmai.com>
2023-09-01 01:09:45 +0300

    add files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-const number = 33;
+// let number = prompt('Enter number');
 
-if (number % 3 === 0 && number % 5 === 0) {
-    console.log('FizzBuzz');
-} else if (number % 3 === 0) {
-  console.log('Fizz');
-} else if (number % 5 === 0) {
-    console.log('Buzz');
-} 
+// if (number % 3 === 0 && number % 5 === 0) {
+//   console.log('FizzBuzz');
+// } else if (number % 3 === 0) {
+//  console.log('Fizz');
+// } else if (number % 5 === 0) {
+//    console.log('Buzz');
+// } 
  
-//var a;
-//a = 2023;
 
-//let accessAllowed;
+// const userInput = prompt('Enter number');
 
-//if (a % 4 === 0) {
-//    accessAllowed = 'Так';
-//} else {
-//    accessAllowed = 'Ні';
-//}
-//console.log(accessAllowed);
+// let accessAllowed;
 
+// if (userInput % 4 == 0 && userInput % 100 !== 0 || userInput % 400 == 0) {
+//     accessAllowed = 'Так';
+// } else {
+//     accessAllowed = 'Ні';
+// }
+// console.log(accessAllowed);
+
+let age = prompt('Скільки Вам років?', '');
+
+if (age % 10 === 1 && age % 100 !== 11) {
+     console.log('Вам ' + age + ' рік!');
+} else if (2 <= age % 10 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+     console.log('Вам ' + age + ' роки!');
+} else {
+    console.log('Вам ' + age + ' років!');
+}
